daff0dbedccc5476a69398586148a055641a5d23
priya kumari <webyzgirl@gmail.com>
2025-05-21 19:00:52 +0530

    first commit

---
+++ routes/shop.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const adminData = require('./admin');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  console.log("this data is coming form shop", adminData.products);
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+
+  // rendering the html file using pug
+  const products = adminData.products || [];
+  res.render('shop',{prods:products,docTitle:'Shop',path:'/'}) 
+});
+
+module.exports = router;
