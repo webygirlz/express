39cef1689abc5dc848868c425b4b69bc0b3bf151
priya kuamri <webygirlz@gmial.com>
2025-05-25 11:18:18 +0530

    ejs templeting engine

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use('/admin',adminRoutes.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404',{pageTitle:'page Not Found'});
+    res.status(404).render('404',{pageTitle:'page Not Found',path:'/404'});
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,19 +1,22 @@
-// const path = require('path');
+const path = require('path');
 
 const express = require('express');
 
 const rootDir = require('../util/path');
-
 const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  console.log("this data is coming form shop", adminData.products);
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-
-  // rendering the html file using pug
-  const products = adminData.products || [];
-  res.render('shop',{prods:products,docTitle:'Shop',path:'/',hasProducts :products.length,activeShop:true,ProductCSS:true,layout:true}) 
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
